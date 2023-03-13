4de78a189eef5c32e07977ed7e9cddba80dcb9a4
Sébastien Chopin <seb@nuxt.com>
2023-03-13 12:48:42 +0100

    fix: use alternativePortRange

--- src/index.ts
+++ src/index.ts
@@ -84,7 +84,7 @@ export async function listen(
     port: Number(options_.port),
     verbose: !options_.isTest,
     host: options_.hostname,
-    portRange: [3000, 3100],
+    alternativePortRange: [3000, 3100],
     ...(typeof options_.port === "object" && options_.port),
   });
 
