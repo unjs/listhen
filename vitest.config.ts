9d41e5df6aa887768a70057e2905b2c60538a86a
ysknsid25 <kengo071225@gmail.com>
2025-04-03 22:18:14 +0900

    test: only include src for coverage report
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ["text", "clover", "json"]
+      reporter: ["text", "clover", "json"],
+      include: ["src/**/*.ts"],
     },
     globals: true,
-    globalSetup: [
-      "./test/setup.ts"
-    ]
-  }
+    globalSetup: ["./test/setup.ts"],
+  },
 });
